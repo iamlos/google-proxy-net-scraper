3fc623711d6d7066ba28c3b415c477ce078f4a1a
Vaidas <shibalba1@gmail.com>
2015-12-21 23:43:39 +0200

    update composer; finish scraper

--- src/GoogleProxyNetScraper.php
+++ src/GoogleProxyNetScraper.php
@@ -3,16 +3,19 @@
 namespace ProxyScraper;
 
 use Curl\Curl;
+use ProxyScraper\Interfaces\ProxyScraperRegex;
 
 class GoogleProxyNetScraper implements ProxyScraperRegex{
 
     private $url = 'http://www.google-proxy.net/';
 
-    public function fetchRegex()
+    public function fetch()
     {
         $curl = new Curl();
 
         $curl->get($this->url);
+
+        $proxies = [];
 
         if($curl->error){
             echo 'Error: ' . $curl->error_code;
@@ -23,14 +26,16 @@ class GoogleProxyNetScraper implements ProxyScraperRegex{
             preg_match_all(
                 "/([\d+\.]+)<\/td><td>(\d+)<\/td><td>[\w\s]+<\/td><td>[\w\s]+<\/td><td>([\w\s]+)<\/td>/",
                 $response,
-                $proxies,
+                $proxiesArray,
                 PREG_SET_ORDER
             );
 
-            foreach($proxies as $key => $proxy){
-
+            foreach($proxiesArray as $key => $proxyIter){
+                $proxies[] = new Proxy($proxyIter[1], $proxyIter[2], str_replace(' proxy', '', $proxyIter[3]));
             }
         }
+
+        return $proxies;
     }
 
 }
